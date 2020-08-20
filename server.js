08af92531d4a1fd3c7f192f468dfcfcf95cdeb79
ktotheH <kcjhill1234@msn.com>
2020-08-20 16:52:49 -0600

    fixing

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+var passport = require("./develop/config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
-var db = require("./models");
+var db = require("./develop/models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
@@ -19,8 +19,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./develop/routes/html-routes.js")(app);
+require("./develop/routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
